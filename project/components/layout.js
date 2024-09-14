d759fee4b575941e1e1fbce7778ad52ec7851614
YourMarketingMastery <yourmarketingmastery@gmail.com>
2024-09-15 00:20:03 +0530

    first commit 10

--- project/components/layout.js
+++ project/components/layout.js
@@ -6,14 +6,12 @@ export default function RootLayout({ children }) {
   return (
     <React.Fragment>
       <div>
-        {/* <Header>
         <Head>
           <title>Al Gharbia</title>
           <meta name="description" content="" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-      </Header> */}
         <main> {children}</main>
 
         <Footer></Footer>
